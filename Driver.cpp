bf23c234329f6375ce8888659aff0a78de370f62
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2020-04-20 15:18:46 -0500

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,18 +1,17 @@
 #include "LinkedList.hpp"
+#include "Student.hpp"
+#include "Room.hpp"
 #include <iostream>
 #include <ctime>
 
 int main()
 {
-    std::srand(std::time(nullptr)); // use current time as seed for random generator
-    LinkedList* ll = new LinkedList();
+    LinkedList* room = new LinkedList();
 
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < 15; i++)
     {
         ll->addEnd(std::rand() % 1000);
     }
-    //ll->display();
-    ll->sort();
-    std::cout << "*******************************************\n";
-    ll->display(); //it should now be sorted
+    ll->display(); 
+    
 }
