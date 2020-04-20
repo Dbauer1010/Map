37e297a238b9c9188c9691cff79bec2580679836
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2020-04-20 14:51:47 -0500

    Update Student.hpp

--- Student.hpp
+++ Student.hpp
@@ -1,2 +1,15 @@
-#include <iostream>
-using namspace std
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    public:
+        void Move(string direction);
+        String Room(string direction);
+        
+
+    private:
+        string direction;
+        
+
+};
